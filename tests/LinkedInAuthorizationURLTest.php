7878974df2d52b46878fe34c61dbc8324d7790be
Grégory Saive <greg@evias.be>
2015-11-06 05:36:04 +0100

    add first test LinkedInAuthorizationURLTest ; add autoload for namespace sociall in composer.json

---
+++ tests/LinkedInAuthorizationURLTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace sociall\Tests;
+
+require_once 'PHPUnit/Autoload.php';
+require_once 'src/LinkedIn.php';
+
+class LinkedInAuthorizationURLTest
+    extends \PHPUnit_Framework_TestCase
+{
+    private $_app_id       = "LinkedIn_App_Id";
+    private $_app_secret   = "LinkedIn_App_Secret";
+    private $_callback_url = "http://example.com/auth/callback";
+
+    public function testArrayInitialization()
+    {
+        $client = new \sociall\LinkedIn([
+            "app_id"     => $this->_app_id,
+            "app_secret" => $this->_app_secret,
+            "auth_callback_url" => $this->_callback_url]);
+
+        $this->assertEquals($client->getAppId(), $this->_app_id);
+        $this->assertEquals($client->getAppSecret(), $this->_app_secret);
+        $this->assertEquals($client->getAuthCallbackURL(), $this->_callback_url);
+    }
+}
+?>
