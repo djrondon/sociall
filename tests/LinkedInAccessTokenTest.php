0ad27ae2e83321ae82f2f2ed0e2841a73aadf3de
Grégory Saive <greg@evias.be>
2015-11-10 02:54:37 +0100

    add license informations to unit tests

--- tests/LinkedInAccessTokenTest.php
+++ tests/LinkedInAccessTokenTest.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * LICENSE
+ *
+ Copyright 2015 Grégory Saive (greg@evias.be)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ *
+ * @package Sociall
+ * @subpackage Tests
+ * @author Grégory Saive <greg@evias.be>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+**/
+
 namespace sociall\Tests;
 
 require_once 'PHPUnit/Autoload.php';
